ba08aaf5cb6b8262be4846906cd2b150dbf6a026
Mitesh-patel7 <pmitesh8@gmail.com>
2025-10-20 14:30:13 +0530

    commit

---
+++ Assgnmt_Arithmetic_Ops.py
@@ -0,0 +1,16 @@
+# User Input
+
+F_I = float(input("Enter the first number: "))
+S_I = float(input("Enter the second number: "))
+
+# Arithmetic Operations
+add = F_I + S_I
+sub = F_I - S_I
+mul = F_I * S_I
+div = F_I / S_I
+
+# result
+print("Addition:", round(add))
+print("Subtraction:",round(sub))
+print("Multiplication:",round(mul))
+print("Division:",div)
